8cd43c839c131785ba2d08e755563d3971984ffa
Pawel Kosiorek <czerwonewierchy1971@gmail.com>
2020-10-31 18:55:34 +0100

    first commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,20 +1,15 @@
-public class Calculator {
+package com.kodilla;
 
+public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
     public int sub(int a, int b) {
         return a - b;
     }
-
     public static void main(String[] args) {
         Calculator c = new Calculator();
-        
-        
-        System.out.println(c.add(11, 13));
-        System.out.println(c.sub(19, 7));
-    
+        System.out.println(c.add(9, 7));
+        System.out.println(c.sub(19, 17));
     }
-    
 }
-
